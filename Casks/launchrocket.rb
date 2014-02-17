51c8180f23771f8a6e7891f1e8f3cf1bae16d7d2
Josh Butts <josh@joshbutts.com>
2014-02-16 23:16:45 -0600

    Update launchrocket.rb

--- Casks/launchrocket.rb
+++ Casks/launchrocket.rb
@@ -2,6 +2,6 @@ class Launchrocket < Cask
   url 'https://github.com/jimbojsb/launchrocket/releases/download/v0.5.1/LaunchRocket.prefPane.zip'
   homepage 'http://guthub.com/jimbojsb/launchrocket'
   version '0.5.1'
-  sha1 '8618dab55cccc37a78977fa304fe2a156b99d029'
+  sha1 '8aab19f421ab15e48e250b3de824eb3d60236e0f'
   prefpane 'LaunchRocket.prefPane'
 end
