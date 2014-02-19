6ccdbf43635276ff3507181415601eeebd709652
Josh Butts <josh@joshbutts.com>
2014-02-18 21:59:58 -0600

    update sha hash

--- Formula/launchrocket-plists.rb
+++ Formula/launchrocket-plists.rb
@@ -3,7 +3,7 @@ require 'net/http'
 
 class LaunchrocketPlists < Formula
   url 'https://github.com/jimbojsb/launchrocket-plists/archive/v0.1.1.zip'
-  sha1 'b9382f0b93f0f62ab0d70f08f6f9a156aa04e122'
+  sha1 'ae3f25a07605d932359c6a2781402dadbc100da3'
   homepage 'http://github.com/jimbojsb/launchrocket-plists'
   version '0.1.1'
 
