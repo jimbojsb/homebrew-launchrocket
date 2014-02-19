f328432c18fce9dd1a23c562f2b9ed3e05a24e2f
Josh Butts <josh@joshbutts.com>
2014-02-18 22:27:43 -0600

    update

--- Formula/launchrocket-plists.rb
+++ Formula/launchrocket-plists.rb
@@ -2,9 +2,15 @@ require 'formula'
 require 'net/http'
 
 class LaunchrocketPlists < Formula
-  head 'https://github.com/jimbojsb/launchrocket-plists.git'
+  url 'https://github.com/jimbojsb/launchrocket-plists/archive/v0.1.1.zip'
+  sha1 'ae3f25a07605d932359c6a2781402dadbc100da3'
+  homepage 'http://github.com/jimbojsb/launchrocket-plists'
+  version '0.1.1'
 
+  
   def install
-    
+    ohai "Installing..."
   end
+  
+  
 end
