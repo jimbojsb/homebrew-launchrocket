8d5a86a252713b78a274f83c9b32053de960f586
Josh Butts <josh@joshbutts.com>
2014-02-18 21:54:52 -0600

    Add plists formula

--- Formulas/launchrocket-plists.rb
+++ Formulas/launchrocket-plists.rb
@@ -1,1 +1,14 @@
-#
+require 'formula'
+require 'net/http'
+
+class LaunchrocketPlists < Formula
+  url 'https://github.com/jimbojsb/launchrocket-plists/archive/v0.1.1.zip'
+  sha1 'b9382f0b93f0f62ab0d70f08f6f9a156aa04e122'
+  homepage 'http://github.com/jimbojsb/launchrocket-plists'
+  version '0.1.1'
+
+  
+  def install
+    
+  end
+end
